37e3b87ededca8da9d4950a2c50cf89c32f524a8
Alexey Potory <>
2023-03-08 06:55:36 +0500

    Добавил операции над объектами

---
+++ AutomaticToolbox.Console/Generation/Operations/BasicOperation.cs
@@ -0,0 +1,18 @@
+﻿using AutomaticToolbox.Console.Generation.Abstract;
+
+namespace AutomaticToolbox.Console.Generation.Operations;
+
+public abstract class BasicOperation<T> : ITransformOperation
+{
+    public object Evaluate(object obj)
+    {
+        if (obj is not T val)
+        {
+            throw new ArgumentException($"The argument provided is not of type {typeof(T)}");
+        }
+
+        return Evaluate(val);
+    }
+
+    protected abstract T Evaluate(T source);
+}
---
+++ AutomaticToolbox.Console/Generation/Operations/MultiplyTransformOperation.cs
@@ -0,0 +1,16 @@
+﻿namespace AutomaticToolbox.Console.Generation.Operations;
+
+public class MultiplyTransformOperation : BasicOperation<double>
+{
+    private readonly double _value;
+
+    public MultiplyTransformOperation(double value)
+    {
+        _value = value;
+    }
+
+    protected override double Evaluate(double source)
+    {
+        return source * _value;
+    }
+}
