329439d87fb874b6bf96897b060ab6e10b8cdd47
Alexey Potory <>
2023-03-08 06:54:34 +0500

    Добавил различные источники для JSON

---
+++ AutomaticToolbox.Console/Generation/Sources/RandLineSource.cs
@@ -0,0 +1,18 @@
+﻿using AutomaticToolbox.Console.Extensions;
+
+namespace AutomaticToolbox.Console.Generation.Sources;
+
+public class RandLineSource : Source
+{
+    private readonly string[] _lines;
+
+    public RandLineSource(string filePath)
+    {
+        _lines = File.ReadAllLines(filePath);
+    }
+
+    public override object GetValue()
+    {
+        return _lines.OneOf();
+    }
+}
